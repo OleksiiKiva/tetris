31ff8b4a3ee840f2b7d230cd035728b8b2578c25
Oleksii Kiva <oleksii.kiva@gmail.com>
2024-06-19 12:20:35 +0300

    feat: create grid & show it

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 
 W, H = 10, 20
-TILE = 45
+TILE = 35
 GAME_RES = W * TILE, H * TILE
 FPS = 60
 
@@ -9,12 +9,20 @@ pygame.init()
 game_sc = pygame.display.set_mode(GAME_RES)
 clock = pygame.time.Clock()
 
+grid = [
+    pygame.Rect(x * TILE, y * TILE, TILE, TILE)
+    for x in range(W)
+    for y in range(H)
+]
+
 while True:
     game_sc.fill(pygame.Color("black"))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
+    # draw grid
+    [pygame.draw.rect(game_sc, (40, 40, 40), i_rect, 1) for i_rect in grid]
 
     pygame.display.flip()
     clock.tick(FPS)
